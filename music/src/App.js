45862342fe51cb275abd0893528d5ca7f0a82a27
Hero Fiennes-Tiffin <hero-fiennes-tiffin@MacBook-Air-Kirill.local>
2022-04-27 16:11:42 +0300

    first 2

---
+++ music/src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <Home />
+  );
+}
+
+export default App;
---
+++ music/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <footer>
+            <div className='header-list'>
+                <li><a>Проверка 1</a></li>
+                <li><a>Проверка 2</a></li>
+                <li><a>Проверка 3</a></li>
+                <li><a>Проверка 4</a></li>
+                <li><a>Проверка 5</a></li>
+            </div>
+        </footer>
+    );
+}
+
+export default Header;
